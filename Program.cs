ad9879d2a1d09f5dac3cf88024782a57afc6b431
Thales Maggot (MrMadaraGames) <kingonetwork2024@gmail.com>
2026-06-08 18:19:53 -0300

    Updated + Translate for Português Brazil Program.cs
    Updated + Translate for Português Brazil

--- Program.cs
+++ Program.cs
@@ -1,27 +1,96 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        Random random = new Random();
-        int secretNumber = random.Next(1, 101);
+        Console.WriteLine("Sistema iniciado");
+        Console.WriteLine("Escolha uma opção:");
+        Console.WriteLine("1 - Jogo de adivinhação");
+        Console.WriteLine("2 - Calculadora simples");
+        Console.WriteLine("3 - Sair");
 
-        int guess = 0;
+        string option = Console.ReadLine();
 
-        Console.WriteLine("Guess the number between 1 and 100!");
-
-        while (guess != secretNumber)
+        switch (option)
         {
-            Console.Write("Your guess: ");
-            guess = Convert.ToInt32(Console.ReadLine());
-
-            if (guess < secretNumber)
-                Console.WriteLine("Too low!");
-            else if (guess > secretNumber)
-                Console.WriteLine("Too high!");
-            else
-                Console.WriteLine("Correct! You win!");
+            case "1":
+                GuessGame();
+                break;
+            case "2":
+                Calculator();
+                break;
+            case "3":
+                Console.WriteLine("Encerrando...");
+                return;
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
         }
+
+        Console.WriteLine("Programa finalizado");
+    }
+
+    static void GuessGame()
+    {
+        Random rand = new Random();
+        int secret = rand.Next(1, 51);
+        int attempt = 0;
+        int tries = 0;
+
+        Console.WriteLine("Adivinhe o número entre 1 e 50");
+
+        while (attempt != secret)
+        {
+            attempt = Convert.ToInt32(Console.ReadLine());
+            tries++;
+
+            if (attempt < secret)
+                Console.WriteLine("Maior");
+            else if (attempt > secret)
+                Console.WriteLine("Menor");
+        }
+
+        Console.WriteLine("Acertou em " + tries + " tentativas");
+    }
+
+    static void Calculator()
+    {
+        List<double> values = new List<double>();
+
+        Console.WriteLine("Quantos números deseja usar?");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            values.Add(Convert.ToDouble(Console.ReadLine()));
+        }
+
+        Console.WriteLine("Escolha operação (+, -, *, /)");
+        string op = Console.ReadLine();
+
+        double result = values[0];
+
+        for (int i = 1; i < values.Count; i++)
+        {
+            switch (op)
+            {
+                case "+":
+                    result += values[i];
+                    break;
+                case "-":
+                    result -= values[i];
+                    break;
+                case "*":
+                    result *= values[i];
+                    break;
+                case "/":
+                    result /= values[i];
+                    break;
+            }
+        }
+
+        Console.WriteLine("Resultado: " + result);
     }
 }
